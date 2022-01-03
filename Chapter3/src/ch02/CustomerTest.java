e3dd201a26aa4d192a633dacacd0f3498eb36316
minjuukim <96512579+minjuukim@users.noreply.github.com>
2022-01-04 00:38:13 +0900

    Rename Chapter3/ch02/CustomerTest.java to Chapter3/src/ch02/CustomerTest.java

--- Chapter3/src/ch02/CustomerTest.java
+++ Chapter3/src/ch02/CustomerTest.java
@@ -5,13 +5,13 @@ public class CustomerTest {
 	public static void main(String[] args) {
 		
 		Customer customerLee = new Customer();
-		customerLee.setCustomerName("ÀÌ¼ø½Å");
+		customerLee.setCustomerName("Å”ÄšÅºÅ™ËÄ¹");
 		customerLee.setCustomerID(10010);
 		customerLee.bonusPoint = 1000;
 		System.out.println(customerLee.showCustomerInfo());
 		
 		VIPCustomer customerKim = new VIPCustomer();
-		customerKim.setCustomerName("±èÀ¯½Å");
+		customerKim.setCustomerName("Ä…ÄÅ”Å»ËÄ¹");
 		customerKim.setCustomerID(10020);
 		customerKim.bonusPoint = 10000;
 		System.out.println(customerKim.showCustomerInfo());
